ed5cb4e410b629d119503ea0b080ce6951e3d011
Sagar746 <tiwarisagar389@gmail.com>
2026-04-30 16:47:08 +0300

    add navigation test for ivysaur page

--- e2e-tests/example.spec.js
+++ e2e-tests/example.spec.js
@@ -7,4 +7,10 @@ describe('Pokedex', () => {
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
   })
+
+  test('navigating to ivysaur page shows chlorophyll', async ({ page }) => {
+    await page.goto('')
+    await page.getByText('ivysaur').click()
+    await expect(page.getByText('chlorophyll')).toBeVisible()
+  })
 })
